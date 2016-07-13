95d7988a8b1436fca88776ee4be6e810e1caad52
Tecaa <fergushog@hotmail.com>
2016-07-13 19:48:10 -0400

    Avance, todos los pasos codificados (excepto 1 intermedio de checks). Eso si, falta hacerlos funcionar bien

--- app/src/main/java/cl/bananaware/hwoc/ImageViewer.java
+++ app/src/main/java/cl/bananaware/hwoc/ImageViewer.java
@@ -17,6 +17,9 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Rect;
+import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
@@ -123,17 +126,125 @@ public class ImageViewer extends Activity {
 
 
 
+        //STEP 1: start Finding outlines in the binary image
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        Mat hierarchy = new Mat();
+        //cv::findContours(inputImg, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+        Imgproc.findContours(dest, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE );
+        // end Finding outlines in the binary image
+
+
+
+        //STEP 2: start selecting outlines
+        List<RotatedRect> rects = new ArrayList<RotatedRect>();
+      //  std::vector<std::vector >::iterator itc = contours.begin();
+        for(int i=0; i<contours.size(); ++i)//while (itc != contours.end())
+        {
+            MatOfPoint mop = contours.get(i);
+            MatOfPoint2f mop2f = mopToMop2f(mop);
+            //MatOfPoint2f mop2f = new MatOfPoint2f(mop);
+            RotatedRect mr = Imgproc.minAreaRect(mop2f); // SE CAE EN ESTA LINEA
+
+            double area = Math.abs(Imgproc.contourArea(contours.get(i)));
+            double bbArea=mr.size.width * mr.size.height;
+            float ratio = (float)(area/bbArea);
+
+            if( (ratio < 0.45) || (bbArea < 400) ){
+                //itc= contours.erase(itc);
+                ;// do nothing
+            }else{
+                rects.add(mr);
+            }
+        }
+        // end selecting outlines
+
+        //STEP 3: loop
+        for (int i=0; i<rects.size(); ++i)
+        {
+
+
+            //STEP 4:
+            double dx = 0.15126 * rects.get(i).boundingRect().size().width;
+            double dy = 0.625* rects.get(i).boundingRect().size().height;;
+            int newWidth = (int)(rects.get(i).boundingRect().size().width + dx);
+            int newHeight = (int)(rects.get(i).boundingRect().size().height + dy);
+
+            //STEP 5:
+            Mat uncropped = src.clone();
+            Rect roi = new Rect(rects.get(i).boundingRect().x, rects.get(i).boundingRect().y, newWidth, newHeight);
+            Mat cropped = new Mat(uncropped, roi);
+
+            //STEP 6:
+            Imgproc.cvtColor(cropped, cropped, Imgproc.COLOR_RGB2GRAY); //Convert to gray scale
+            Imgproc.threshold(cropped, cropped, 0, 255, Imgproc.THRESH_OTSU | Imgproc.THRESH_BINARY);
+
+            //STEP 7:
+            List<MatOfPoint> contours2 = new ArrayList<MatOfPoint>();
+            Mat hierarchy2 = new Mat();
+            Imgproc.findContours(cropped, contours2, hierarchy2, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE );
+
+            //STEP 8:
+            double maxArea = 0;
+            int maxAreaIndex = 0;
+            for(int j=0; j<contours2.size(); ++j)//while (itc != contours.end())
+            {
+                MatOfPoint mop = contours2.get(j);
+                MatOfPoint2f mop2f = mopToMop2f(mop);
+                //MatOfPoint2f mop2f = new MatOfPoint2f(mop);
+                RotatedRect mr = Imgproc.minAreaRect(mop2f); // SE CAE EN ESTA LINEA
+
+                double currentArea=mr.size.width * mr.size.height;
+                if (currentArea > maxArea) {
+                    maxArea = currentArea;
+                    maxAreaIndex = j;
+                }
+            }
+
+            //STEP 9:
+            MatOfPoint mop2 = contours.get(maxAreaIndex);
+            MatOfPoint2f mop2f2 = mopToMop2f(mop2);
+            //MatOfPoint2f mop2f = new MatOfPoint2f(mop);
+            RotatedRect mr2 = Imgproc.minAreaRect(mop2f2); // SE CAE EN ESTA LINEA
+
+            //STEP 10:
+            //checks
+
+            //STEP 11:
+            Mat sinCortar = src.clone();
+            Rect roi2 = new Rect(mr2.boundingRect().x, mr2.boundingRect().y, (int)mr2.size.width, (int)mr2.size.height);
+            Mat cropped2 = new Mat(sinCortar, roi2);
+
+
+            if (i==2) {
+                // mostrar en pantalla
+                Mat finalMat1 = cropped2;
+                Bitmap bm1 = Bitmap.createBitmap(finalMat1.cols(), finalMat1.rows(), Bitmap.Config.ARGB_8888);
+                Utils.matToBitmap(finalMat1, bm1);
+                imgFp.setImageBitmap(bm1);
+            }
+
+        }
+
+
+
+
+/*
         Mat finalMat = dest;
         Bitmap bm = Bitmap.createBitmap(finalMat.cols(), finalMat.rows(), Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(finalMat, bm);
         imgFp.setImageBitmap(bm);
 
-
+*/
 
 
 
         //savedFuncion();
     }
+
+    private MatOfPoint2f mopToMop2f(MatOfPoint mop) {
+        return new MatOfPoint2f( mop.toArray() );
+    }
+
     private void sub(Mat src1, Mat src2, Mat src3)
     {
         Core.subtract(src1, src2, src3);
